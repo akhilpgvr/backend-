ed979918ab25e53a750875ac7acd8f4b955e41a9
akhilpgvr <akhilpgvr@gmail.com>
2026-05-27 23:19:21 +0530

    adding Order entity

--- src/main/java/com/noblewalk/backend/model/entity/OrderEntity.java
+++ src/main/java/com/noblewalk/backend/model/entity/OrderEntity.java
@@ -1,4 +1,37 @@
 package com.noblewalk.backend.model.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Generated;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Date;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "appointments")
 public class OrderEntity {
+
+    @Id
+    @Generated
+    private String id;
+
+    private String userMobileNo;
+    private String userMailId;
+    private String userName;
+    private List<String> ItemNames;
+    private Double totalPrice;
+    private String deliveryAddress;
+    private Date orderedDate;
+
+    private LocalDateTime createdOn;
+    private String createdBy;
+    private LocalDateTime updatedOn;
+    private String updatedBy;
 }
